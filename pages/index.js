5a246d11d3111eedd0245f1ea88190760ceee8b6
Ronix666 <43515632+Ronix666@users.noreply.github.com>
2022-03-27 18:42:24 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 export default function Home() {
   return (
@@ -10,9 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
-        
+        <Header title="Gelff" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
       </main>
 
+      <Footer />
     </div>
   )
 }
